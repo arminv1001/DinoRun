10f3d6a497f3fb84b4c06d3773426c49ed0139cc
Armin Vosoghi Marand <arminvosoghi@gmail.com>
2021-05-06 00:36:06 +0200

    Welt mit Schwierigkeitsgrad + Bug Fix mit -> pygame.time.delay(60)

--- World.py
+++ World.py
@@ -2,13 +2,14 @@ import pygame
 
 class World:
 
-    def __init__(self,WIDTH,HEIGHT):
+    def __init__(self,WIDTH,HEIGHT,difficulty):
         self.background = pygame.image.load("images/Hintergrund/background.jpg")
         self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
         self.width = WIDTH
         self.height = HEIGHT
         self.background1X = 0
         self.background2X = self.width
+        self.difficulty = difficulty
 
     def placeBackground(self,window):
         """
@@ -16,11 +17,11 @@ class World:
         :param window: Pygames Fenster
         :return: -
         """
-        self.background1X -= 1
-        self.background2X -= 1
-        if self.background1X == (self.width * -1):
+        self.background1X -= 1 *  self.difficulty
+        self.background2X -= 1 * self.difficulty
+        if self.background1X <= (self.width * -1):
             self.background1X = self.width
-        if self.background2X == (self.width * -1):
+        if self.background2X <= (self.width * -1):
             self.background2X = self.width
         window.blit(self.background, (self.background1X, 0))
         window.blit(self.background, (self.background2X,0))
--- gameLoop.py
+++ gameLoop.py
@@ -8,13 +8,16 @@ WIDTH = 800
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 run = True
-
+clock = pygame.time.Clock()
 #Hintergrund laden
-backgroundWorld = World(WIDTH,HEIGHT)
+# TODO Schwierigkeit mit der Zeit erhöhen oder am anfang aushwälen?
+backgroundWorld = World(WIDTH,HEIGHT,1)
 
 while run:
+    # TODO pygame.time.delay(60) brauchst du das??? -> wenn ja dann probiers mal mit dem clock tick,
+    #  weil glaub des war die falsche Funktion die du da verwendet hast
     # bestimmt die Frames per second
-    pygame.time.delay(60)
+    # clock.tick(60)
 
     #Background
     backgroundWorld.placeBackground(win)
