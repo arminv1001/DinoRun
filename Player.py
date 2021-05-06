805c9ddc6500234a52e020dfa7ae3074f7c326a7
FinnFreiheit <finn.owe.freiheit@gmail.com>
2021-05-06 12:23:09 +0200

    player move

--- Player.py
+++ Player.py
@@ -1,24 +1,40 @@
 import pygame
 
+
 class Player:
 
-    def __init__(self,x,y,step):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, step, difficulty):
+        self.x = x - 48
+        self.y = y - 64
         self.step = step
-
+        self.difficulty = difficulty
+        self.left = False
+        self.right = False
         self.walkIndex = 0
+        self.loadImage()
 
-    def left(self):
-        self.x -= self.step
+    def draw(self, window):
+        if self.walkIndex + 1 >= 42:
+            self.walkIndex = 0
 
-    def right(self):
-        self.x += self.step
-
-    def up(self):
-        self.y -= self.step
+        if self.left:
+            window.blit(self.walkLeft[self.walkIndex // 6], (self.x, self.y))
+            if self.x >= 0:
+                self.x -= self.step
+                self.walkIndex += 1
+        elif self.right:
+            # TODO muss besser
+            window.blit(self.walkRight[self.walkIndex // 6], (self.x, self.y))
+            if self.x <= 700:
+                self.x += self.step
+                self.walkIndex += 1
+        else:
+            window.blit(self.standing, (self.x, self.y))
+            if self.x >= 0:
+                self.x -= self.difficulty
 
     def loadImage(self):
+        # Bildgroeße: 96x128
         self.walkRight = [pygame.image.load('Images/SpielerImages/Right/R0.png'),
                           pygame.image.load('Images/SpielerImages/Right/R1.png'),
                           pygame.image.load('Images/SpielerImages/Right/R2.png'),
@@ -28,11 +44,13 @@ class Player:
                           pygame.image.load('Images/SpielerImages/Right/R6.png'),
                           pygame.image.load('Images/SpielerImages/Right/R7.png')]
 
-        self.walkLeft =  [pygame.image.load('Images/SpielerImages/Left/L0.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L1.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L2.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L3.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L4.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L5.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L6.png'),
-                          pygame.image.load('Images/SpielerImages/Left/L7.png')]
+        self.walkLeft = [pygame.image.load('Images/SpielerImages/Left/L0.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L1.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L2.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L3.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L4.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L5.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L6.png'),
+                         pygame.image.load('Images/SpielerImages/Left/L7.png')]
+
+        self.standing = pygame.image.load('Images/SpielerImages/standing.png')
--- gameLoop.py
+++ gameLoop.py
@@ -1,34 +1,69 @@
 import pygame
 from World import World
-
-def drawGame(window):
-    backgroundWorld.placeBackground(window)
-    window.blit(backgroundWorld.background, (backgroundWorld.background1X, 0))
-    window.blit(backgroundWorld.background, (backgroundWorld.background2X, 0))
-    pygame.display.update()
+from Player import Player
 
 pygame.init()
+
+'''
+Konstanten definieren 
+'''
 
 HEIGHT = 500
 WIDTH = 800
 DIFFICULTY = 2
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
+
 run = True
 clock = pygame.time.Clock()
-#Hintergrund laden
-backgroundWorld = World(WIDTH,HEIGHT,DIFFICULTY)
 
+# Hintergrund laden
+# Fußboden bei 350
+backgroundWorld = World(WIDTH, HEIGHT, DIFFICULTY)
+man = Player(WIDTH / 2, HEIGHT - 150, DIFFICULTY, DIFFICULTY)
+
+'''
+Gameloop Funktionen
+'''
+
+
+def drawGame(window):
+    backgroundWorld.placeBackground(window)
+    window.blit(backgroundWorld.background, (backgroundWorld.background1X, 0))
+    window.blit(backgroundWorld.background, (backgroundWorld.background2X, 0))
+    man.draw(window)
+    pygame.display.update()
+
+
+'''
+Gameloop 
+'''
 while run:
     # bestimmt die Frames per second
     clock.tick(60)
 
-    #Background
-    drawGame(win)
-    # Auf die angaben des Benutzers reagieren
-    for event in pygame.event.get():
+    # Background
 
+
+    for event in pygame.event.get():
         # Fenster schließen
         if event.type == pygame.QUIT:  # Checks if the red button in the corner of the window is clicked
             run = False  # Ends the game loop
+
+    # Key Steuerung Player
+    # TODO eventuel auslagern
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < WIDTH:
+        man.right = True
+        man.left = False
+    else:
+        man.right = False
+        man.left = False
+
+    drawGame(win)
+
+
 pygame.quit()
