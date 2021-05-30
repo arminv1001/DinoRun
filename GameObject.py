691be138bc135241af90e17bd2a546775126b44e
Armin Vosoghi Marand <arminvosoghi@gmail.com>
2021-05-30 15:49:44 +0200

    Vererbung

--- GameObject.py
+++ GameObject.py
@@ -4,13 +4,16 @@ import random
 
 class GameObject:
 
-    def __init__(self, WIDTH, HEIGHT, difficulty):
-        self.width = WIDTH
-        self.height = HEIGHT
+    def __init__(self, WIDTH, HEIGHT, difficulty,imageObj, widthObj, heightObj):
+        self.widthFrame = WIDTH
+        self.heightFrame = HEIGHT
+        self.widthObj = widthObj
+        self.heightObj = heightObj
         self.difficulty = difficulty
         self.X = 0
         self.Y = 0
-
+        self.image = pygame.image.load(imageObj)
+        self.hitbox = pygame.Rect(self.X,self.Y, self.widthObj,self.heightObj)
 
     def setX(self, x):
         self.X = x
@@ -29,5 +32,5 @@ class GameObject:
         """
         :return:
         """
-        self.rect = pygame.Rect(self.X, self.Y, self.width, self.height)
-        pygame.draw.rect(window, (255, 0, 0), self.rect, 2)
+        self.hitbox = pygame.Rect(self.X, self.Y, self.widthFrame, self.heightFrame)
+        pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
