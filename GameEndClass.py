fbe87238594412a1aa210531ee3c42afdbb7522a
Armin Vosoghi Marand <arminvosoghi@gmail.com>
2021-06-02 19:18:46 +0200

    DB Part 1

--- GameEndClass.py
+++ GameEndClass.py
@@ -2,19 +2,23 @@ import pygame
 
 from GamePage import GamePage
 from World import World
-
+from HighScoreDB import HighScoreDB
 
 class GameEndClass(GamePage):
 
     def __init__(self, WIDTH, HEIGHT, DIFFICULTY):
 
         super().__init__(WIDTH, HEIGHT, DIFFICULTY)
-
+        self.gameFont = pygame.font.Font(self.font,20)
         self.mouseHover = False
+        self.highscoreDB = HighScoreDB()
+        self.highscoreDBContent = self.highscoreDB.returnHighscoreList()
 
 
-
-
+    def printFrame(self, text, heightText,fontSize):
+        textFormat = self.textFormat(text, self.font, fontSize, self.black)
+        textRect = textFormat.get_rect()
+        self.win.blit(textFormat, ((self.WIDTH / 2 - textRect[2]/ 2), heightText))
 
     def draw(self):
         #self.backgroundWorld.placeBackground(self.win)
@@ -27,18 +31,19 @@ class GameEndClass(GamePage):
             textStart = self.textFormat("RESTART", self.font, 75, self.white)
 
         startRect = textStart.get_rect()
-        koord = (self.WIDTH / 2 - (startRect[2] / 2), 300)
+        koord = (self.WIDTH / 2 - (startRect[2] / 2), 400)
         self.startRect = startRect.move(koord)
         self.win.blit(textStart, koord)
 
-        textTitle = self.textFormat("Game Over", self.font, 75, self.black)
-        titleRect = textTitle.get_rect()
-        self.win.blit(textTitle, (self.WIDTH / 2 - (titleRect[2] / 2), 50))
-
+        self.printFrame("Game Over", 10,75)
+        self.printFrame("Highscore:", 100,50)
+        # TODO PLOT HIGHSCORE
+        pygame.draw.rect(self.win, self.black,(self.WIDTH/2 - 200, 200, 400,40),2)
         pygame.display.update()
 
 
     def loop(self):
+        user_text = ""
         while True:
             self.draw()
             for event in pygame.event.get():
@@ -52,3 +57,19 @@ class GameEndClass(GamePage):
                     self.mouseHover = False
                 if event.type == pygame.MOUSEBUTTONUP and self.mouseHover:
                     return True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_text += user_text[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        print("ENTER")
+                        # TODO Upload DB
+                    else:
+                        if (len(user_text) < 20):
+                            user_text += event.unicode
+
+
+                text_surface = self.gameFont.render(user_text,True,self.black)
+                self.win.blit(text_surface,(self.WIDTH/2 - 120,205))
+
+                pygame.display.flip()
+                self.clock.tick(60)
